2f1b096ee8571f4410c9f3aa6cc509504b08a4c8
m48483 <71882579+m48483@users.noreply.github.com>
2024-05-15 16:45:24 +0900

    comment list

--- src/pages/CommentList.jsx
+++ src/pages/CommentList.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import CreateComment from "./CreateComment";
-import Comment from "../components/Comment";
 export default function CommentList() {
   return (
     <>
       <section style={{ backgroundColor: "#eee" }}>
-        <Comment />
         <div className="container my-5 py-5">
           <div className="row d-flex justify-content-center">
             <div className="col-md-12 col-lg-10 col-xl-8">
